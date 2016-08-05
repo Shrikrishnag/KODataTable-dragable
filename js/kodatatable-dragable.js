86e3e65d7a6c69e987dce9f0a7ee5a24fcf6b2e9
Geerts, Ryan J <ryan-geerts@uiowa.edu>
2016-08-05 15:18:59 -0500

    master - better column visibility support

--- js/kodatatable-dragable.js
+++ js/kodatatable-dragable.js
@@ -15,13 +15,14 @@
         var tableSelector = doptions.tableSelector;
         var koTable = this;
         var defaultDragOptions = {
-            persistState: function(table) {
+            persistState: function() {
+                var sortOrder = {};
                 if (!window.localStorage) return;
                 var ss = window.localStorage;
                 $.each(koTable.columns(),function(i) {
-                    if(this.id != '') {table.sortOrder[this.id]=i;}
+                    if(this.id != '') {sortOrder[this.id]=i;}
                 });
-                ss.setItem(tableSelector + '-tableorder',JSON.stringify(table.sortOrder));
+                ss.setItem(tableSelector + '-tableorder',JSON.stringify(sortOrder));
             },
             restoreState: function(){
                 var sortOrder = JSON.parse(window.localStorage.getItem(tableSelector +'-tableorder'));
@@ -31,44 +32,47 @@
                 koTable.columns.sort(function(left,right){
                     var newleft = sortOrder[left.id];
                     var newright = sortOrder[right.id];
-                    return newleft === newright ? 0 : (newleft < newright ? -1 : 1)
+                    return newleft === newright ? 0 : (newleft < newright ? -1 : 1);
 
                 });
             }
         };
         var dragtable = $(tableSelector).dragtable($.extend(defaultDragOptions, doptions)).dragtable("instance");
         koTable.dragtableInstance = dragtable;
-        dragtable._bubbleCols = function() {
+        dragtable._bubbleCols = function(startEndVisibilityAccountedFor) {
             var table = dragtable.originalTable;
             var columnsReordered = [];
             var origColumns = [];
 
             //my version of ko datatables has visibleColumns
-            var tmpCols = [];
-            if(koTable.visibleColumns){
-                origColumns = koTable.visibleColumns();
-            } else {
-                origColumns = koTable.columns().splice(0);
+            var startIndex = table.startIndex;
+            var endIndex = table.endIndex;
+            if(!startEndVisibilityAccountedFor && koTable.visibleColumns) {
+                startIndex = koTable.columns().indexOf(koTable.visibleColumns()[startIndex-1])+1;
+                endIndex = koTable.columns().indexOf(koTable.visibleColumns()[endIndex-1])+1;
             }
 
-            if (table.startIndex === table.endIndex) {
+            var origColumns = koTable.columns().slice(0);
+
+
+            if (startIndex === endIndex) {
                 return true;
-            } else if (table.startIndex > table.endIndex) {
-                $.each(origColumns.splice(0, table.endIndex - 1), function (_, i) {
+            } else if (startIndex > endIndex) {
+                $.each(origColumns.splice(0, endIndex - 1), function (_, i) {
                     columnsReordered.push(i)
                 });
-                columnsReordered.push(origColumns[table.startIndex - table.endIndex]);
-                $.each(origColumns.splice(0, table.startIndex - table.endIndex), function (_, i) {
+                columnsReordered.push(origColumns[startIndex - endIndex]);
+                $.each(origColumns.splice(0, startIndex - endIndex), function (_, i) {
                     columnsReordered.push(i)
                 });
                 $.each(origColumns.splice(1), function (_, i) {
                     columnsReordered.push(i)
                 });
             } else {
-                $.each(origColumns.splice(0, table.startIndex - 1), function (_, i) {
+                $.each(origColumns.splice(0, startIndex - 1), function (_, i) {
                     columnsReordered.push(i)
                 });
-                $.each(origColumns.splice(1, table.endIndex - table.startIndex), function (_, i) {
+                $.each(origColumns.splice(1, endIndex - startIndex), function (_, i) {
                     columnsReordered.push(i)
                 });
                 $.each(origColumns.splice(0), function (_, i) {
@@ -88,31 +92,133 @@
 
             });
             $(window).resize();
-        }
+        };
+        return dragtable;
     };
 
 
     Kodt.prototype.keyDownMoveColumn = function(column, event) {
         var self = this;
         var ARROW_LEFT = 37, ARROW_RIGHT = 39;
-        var oldIndex = self.visibleColumns().indexOf(column) + 1;
-        var newIndex =  oldIndex;
-        if ( event.keyCode === ARROW_LEFT && oldIndex > 1) {
-            newIndex = oldIndex - 1;
-        } else if ( event.keyCode === ARROW_LEFT && oldIndex === 1){
-            newIndex = self.visibleColumns().length;
-        } else if(event.keyCode === ARROW_RIGHT && oldIndex < self.visibleColumns().length ){
-            newIndex = oldIndex + 1;
-        } else if(event.keyCode === ARROW_RIGHT && oldIndex === self.visibleColumns().length ){
-            newIndex = 1;
+        if((column.visible !== undefined && column.visible() === false) || (column.alwayHidden === undefined && ko.unwrap(column.alwaysHidden) === true)){
+            return true;
         }
-        if(oldIndex !== newIndex){
-            self.dragtableInstance.originalTable.startIndex = oldIndex;
-            self.dragtableInstance.originalTable.endIndex = newIndex;
-            self.dragtableInstance._bubbleCols();
-            $(event.currentTarget).focus();
+        if ( event.keyCode === ARROW_LEFT) {
+            self.moveColumnLeft(column, event, true);
+            return false;
+        } else if(event.keyCode === ARROW_RIGHT) {
+            self.moveColumnRight(column, event, true);
             return false;
         }
+
+        return true;
+    };
+
+    Kodt.prototype.nextVisibleNonHiddenColumn = function(column){
+        var self = this;
+        var origIndex = self.columns.indexOf(column);
+        for(var i = origIndex+1; i !== origIndex && i < 10000; i++){
+            if(i === self.columns().length){
+                i = 0;
+            }
+            if(i === origIndex){
+                return origIndex;
+            }
+            var tmpColumn = self.columns()[i];
+            if((tmpColumn.visible === undefined || tmpColumn.visible()) && (tmpColumn.alwaysHidden === undefined || ko.unwrap(tmpColumn.alwaysHidden) === false) ){
+                return i;
+            }
+        };
+        return origIndex;
+    };
+
+    Kodt.prototype.nextNonHiddenColumn = function(column){
+        var self = this;
+        var origIndex = self.columns.indexOf(column);
+        for(var i = origIndex+1; i !== origIndex && i < 10000; i++){
+            if(i === self.columns().length){
+                i = 0;
+            }
+            if(i === origIndex){
+                return origIndex;
+            }
+            var tmpColumn = self.columns()[i];
+            if(tmpColumn.alwaysHidden === undefined || ko.unwrap(tmpColumn.alwaysHidden) === false ){
+                return i;
+            }
+        };
+        return origIndex;
+    };
+
+    Kodt.prototype.previousVisibleNonHiddenColumn = function(column){
+        var self = this;
+        var origIndex = self.columns.indexOf(column);
+        for(var i = origIndex-1; i !== origIndex && i > -10000; i--){
+            if(i === -1){
+                i = self.columns().length - 1;
+            }
+            if(i === origIndex){
+                return origIndex;
+            }
+            var tmpColumn = self.columns()[i];
+            if((tmpColumn.visible === undefined || tmpColumn.visible()) && (tmpColumn.alwaysHidden === undefined || ko.unwrap(tmpColumn.alwaysHidden) === false) ){
+                return i;
+            }
+        };
+        return origIndex;
+    };
+
+    Kodt.prototype.previousNonHiddenColumn = function(column){
+        var self = this;
+        var origIndex = self.columns.indexOf(column);
+        for(var i = origIndex-1; i !== origIndex && i > -10000; i--){
+            if(i === -1){
+                i = self.columns().length - 1;
+            }
+            if(i === origIndex){
+                return origIndex;
+            }
+            var tmpColumn = self.columns()[i];
+            if(tmpColumn.alwaysHidden === undefined || ko.unwrap(tmpColumn.alwaysHidden) === false ){
+                return i;
+            }
+        };
+        return origIndex;
+    };
+
+    Kodt.prototype.moveColumnRight = function(column, event, visible) {
+        var self = this;
+        var oldIndex = self.columns().indexOf(column) + 1;
+        var newIndex = oldIndex;
+        if(visible){
+            newIndex =  self.nextVisibleNonHiddenColumn(column) + 1;
+        } else {
+            newIndex = self.nextNonHiddenColumn(column) + 1;
+        }
+
+        self.dragtableInstance.originalTable.startIndex = oldIndex;
+        self.dragtableInstance.originalTable.endIndex = newIndex;
+        self.dragtableInstance._bubbleCols(true);
+        self.dragtableInstance.options.persistState();
+        $(event.currentTarget).focus();
+
+        return true;
+    };
+    Kodt.prototype.moveColumnLeft = function(column, event, visible) {
+        var self = this;
+        var oldIndex = self.columns().indexOf(column) + 1;
+        var newIndex = oldIndex;
+        if(visible){
+            newIndex =  self.previousVisibleNonHiddenColumn(column) + 1;
+        } else {
+            newIndex = self.previousNonHiddenColumn(column) + 1;
+        }
+
+        self.dragtableInstance.originalTable.startIndex = oldIndex;
+        self.dragtableInstance.originalTable.endIndex = newIndex;
+        self.dragtableInstance._bubbleCols(true);
+        self.dragtableInstance.options.persistState();
+        $(event.currentTarget).focus();
 
         return true;
     };
